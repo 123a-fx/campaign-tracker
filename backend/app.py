cdb7535c7313cea949cef3f246b258fa410be082
Swasthika M <monapparai663@gmail.com>
2025-11-09 16:34:50 +0530

    commit message

--- backend/app.py
+++ backend/app.py
@@ -90,7 +90,8 @@ def login():
         return jsonify({"success": True})
     return jsonify({"success": False}), 401
 
-
 if __name__ == "__main__":
-   
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
